cbf56a00cfa462779d19966b8c81f6e18c47b630
Smidqe <smidqe@hotmail.fi>
2017-06-26 19:18:03 +0300

    added userscript

---
+++ js/berrytube/smidqetweaks.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        SmidqeTweaks
+// @namespace   SS
+// @description SmidqeTweaks for BT
+// @include     http://berrytube.tv/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    var script = document.createElement("script");
+    script.setAttribute('src', 'https://smidqe.github.io/js/berrytube/stweaks.js');
+    document.head.appendChild(script);
+
+
+})();
