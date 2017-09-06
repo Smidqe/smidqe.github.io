f9cfbc4c5ce8e413b28be083a242f2f28b7596a5
Smidqe <smidqe@hotmail.fi>
2017-09-06 16:55:35 +0300

    simplify titlewrap

--- js/berrytube/smidqeTweaks2/scripts/titleWrap.js
+++ js/berrytube/smidqeTweaks2/scripts/titleWrap.js
@@ -7,40 +7,24 @@ function load() {
             key: 'titleWrap',
             tweak: true,
         }],
-        observer: null,
-        requires: ['listeners'],
-        callback: (node) => {
-            if ($(node).attr('id') === 'berrytweaks-video_title') {
-                self.enable();
-                self.listeners.stop(self.observer);
-            }
-        }
         disable: () => {
             $("#berrytweaks-video_title").unwrap();
             $(".st-window-users").removeClass("wrap");
         },
         enable: () => {
+            //don't stack it
+            if ($('#st-videotitle-window')[0])
+                return;
+
             $("#berrytweaks-video_title").wrap($("<div>", { id: "st-videotitle-window" }));
             $("#st-videotitle-window").addClass("active");
             $(".st-window-users").addClass("wrap");
         },
         init: () => {
-            self.listeners = SmidqeTweaks.modules.listeners;
-
-            //no need to load these settings
-            if (!SmidqeTweaks.settings.get('titleWrap'))
-                return;
-
-            self.observer = {
-                monitor: 'added',
-                path: '#chatControls',
-                config: { childList: true },
-            }
-
-            self.observer.callback = self.callback;
-
-            self.listeners.load(self.observer);
-            self.listeners.run(self.observer);
+            SmidqeTweaks.patch(BerryTweaks, 'enableModule', (name) => {
+                if (name === 'videoTitle' && SmidqeTweaks.settings.get('titleWrap'))
+                    self.enable();
+            });
         },
     }
 
