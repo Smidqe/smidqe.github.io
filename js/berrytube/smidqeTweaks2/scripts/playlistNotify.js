7619b7fdfa1ded46dac5581bda3b8c0dae49b98c
Smidqe <smidqe@hotmail.fi>
2017-09-06 10:36:00 +0300

    Fix playlistNotify again

--- js/berrytube/smidqeTweaks2/scripts/playlistNotify.js
+++ js/berrytube/smidqeTweaks2/scripts/playlistNotify.js
@@ -259,6 +259,9 @@ function load() {
         enable: () => {
             self.playlist = SmidqeTweaks.getModule('playlist', 'main');
             self.listeners = SmidqeTweaks.getModule('listeners', 'main');
+
+            console.log(self.listeners);
+
             self.observer = {
                 monitor: 'all',
                 config: { childList: true, attributes: true, characterData: true, subtree: true, attributeOldValue: true },
@@ -268,8 +271,8 @@ function load() {
                 }
             }
 
-            self.listeners.load(obs);
-            self.listeners.start(obs);
+            self.listeners.load(self.observer);
+            self.listeners.start(self.observer);
         },
         disable: () => {
             self.listener.disconnect();
