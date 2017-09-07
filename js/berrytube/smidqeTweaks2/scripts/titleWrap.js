80b2cdf33be93f36557181b85170d50ddaaba2a8
Smidqe <smidqe@hotmail.fi>
2017-09-07 16:04:16 +0300

    refactor titlewrap

--- js/berrytube/smidqeTweaks2/scripts/titleWrap.js
+++ js/berrytube/smidqeTweaks2/scripts/titleWrap.js
@@ -9,21 +9,7 @@ function load() {
         requires: ['listeners'],
         observer: null,
         wrapped: false,
-        wrap: (node) => {
-            console.log(node);
 
-            if ($(node).attr('id') !== 'berrytweaks-video_title')
-                return;
-
-            if (self.observer.obs)
-                self.listeners.stop(self.observer);
-
-            $("#berrytweaks-video_title").wrap($("<div>", { id: "st-videotitle-window" }));
-            $("#st-videotitle-window").addClass("active");
-            $(".st-window-users").addClass("wrap");
-
-            self.wrapped = true;
-        },
         disable: () => {
             $("#berrytweaks-video_title").unwrap();
             $(".st-window-users").removeClass("wrap");
@@ -34,7 +20,14 @@ function load() {
             if (self.wrapped)
                 return;
 
-            self.wrap();
+            $("#berrytweaks-video_title").wrap($("<div>", { id: "st-videotitle-window" }));
+            $("#st-videotitle-window").addClass("active");
+            $(".st-window-users").addClass("wrap");
+
+            if (self.observer.obs)
+                self.listeners.stop(self.observer);
+
+            self.wrapped = true;
         },
         init: () => {
             self.listeners = SmidqeTweaks.getModule('listeners', 'main');
@@ -44,16 +37,11 @@ function load() {
                 config: { childList: true }
             }
 
-            self.observer.callback = self.wrap;
+            self.observer.callback = self.enable;
 
-            if (!window.BerryTweaks) {
+            if (SmidqeTweaks.settings.get('titleWrap')) {
                 self.listeners.load(self.observer);
                 self.listeners.start(self.observer);
-            } else {
-                SmidqeTweaks.patch(BerryTweaks, 'enableModule', (name) => {
-                    if (name === 'videoTitle' && SmidqeTweaks.settings.get('titleWrap'))
-                        self.enable();
-                });
             }
         },
     }
