720ff1f1a2d9c40aac3d75df5829418b3f2195b8
Smidqe <smidqe@hotmail.fi>
2017-09-13 20:01:17 +0300

    fix typo

--- js/berrytube/smidqeTweaks2/modules/time.js
+++ js/berrytube/smidqeTweaks2/modules/time.js
@@ -37,7 +37,7 @@ function load() {
 
         init: () => {
             self.stats = SmidqeTweaks.modules.stats;
-            self.stats.addPair(self.stats.getBlock('general'), pair)
+            self.stats.addPair(self.stats.getBlock('general'), self.pair)
 
             setInterval(() => {
                 $("#st-info-time > span").text(self.get());
