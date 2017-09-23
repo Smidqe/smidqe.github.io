aca210d0e65ffe5db025a3c370a6cdc83ded7389
Smidqe <smidqe@hotmail.fi>
2017-09-23 16:06:58 +0300

    refix

--- js/berrytube/smidqeTweaks2/modules/menu.js
+++ js/berrytube/smidqeTweaks2/modules/menu.js
@@ -136,7 +136,7 @@ function load() {
 
             $.each(self.names.groups, (category, value) => {
                 $.each(value, (index, name) => {
-                    self.addGroup({ category: category.toLowerCase(), id: name })
+                    self.addGroup({ category: category.toLowerCase(), id: name.toLowerCase(), text: name })
                 })
             })
 
