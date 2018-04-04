322b1ffcdf009db6cd2527ab318f8826149e0782
Smidqe <smidqe@hotmail.fi>
2018-04-04 18:10:13 +0300

    Add missing files

---
+++ js/berrytube/smidqeTweaks2/scripts/playlistControls.js
@@ -0,0 +1,69 @@
+function load() {
+	const self = {
+		meta: {
+			name: 'playlistControls',
+			group: 'scripts',
+			requires: ['windows'],
+		},
+		settings: {
+			group: 'playlist',
+			values: [{
+				title: 'Make poll controls modular',
+				key: 'playlistControls',
+				depends: ['layout']
+			},{
+				title: 'Force open on berry',
+				key: 'playlistControlsBerry',
+				depends: ['layout', 'playlistControls'],
+				sub: true,
+			},{
+				title: 'Force open on login',
+				key: 'playlistControlsLogin',
+				depends: ['layout', 'playlistControls'],
+				sub: true,
+			}]
+		},
+		windows: null,
+		open: () => {
+			let allow = false;
+
+			if (self.admin && SmidqeTweaks.settings.get('playlistControlsLogin'))
+				allow = true;
+
+			if (window.LEADER && SmidqeTweaks.settings.get('playlistControlsBerry'))
+				allow = true;
+
+			if (!allow)
+			{
+				$('#playlistAddControls').hide();
+				return;
+			}
+
+			if (!self.windows.isModular('playlist'))
+				self.windows.modularize('playlist', true);
+
+			self.windows.show('playlist', true);
+		},
+		enable: () => {
+			if (!self.admin)
+				socket.on('handleACL', self.open);
+			else
+				self.open();
+		},
+		disable: () => {
+			socket.removeListener('handleACL', self.open)
+
+			if (self.windows.isModular('playlist'))
+				self.windows.unmodularize('playlist');
+		},
+		init: () => {
+			self.windows = SmidqeTweaks.get('windows', 'modules');
+			self.admin = window.TYPE >= 1;
+			
+		},
+	}
+
+	return self;
+}
+
+SmidqeTweaks.add(load())
---
+++ js/berrytube/smidqeTweaks2/scripts/wutColors.js
@@ -0,0 +1,31 @@
+/* 
+	TODO:
+		- Move the refresh button to menu
+		- Automate the refresh upon theme change
+			* Need to figure out what happens when it happens
+
+*/
+
+function load() {
+	self = {
+		meta: {
+
+		},
+
+		move: (where) => {
+
+		},
+
+		refresh: () => {
+			
+		},
+
+		init: () => {
+
+		},
+	}
+
+	return self;
+}
+
+SmidqeTweaks.add(load());
